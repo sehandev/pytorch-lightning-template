7a747ef7a3ee0ffe362dfcad697433a80375ad3a
sehandev <sehandev@gmail.com>
2021-09-06 11:30:41 +0900

    FEAT: config

---
+++ config.py
@@ -0,0 +1,40 @@
+# Standard
+from pathlib import Path
+
+# PIP
+import pytorch_lightning as pl
+
+# Custom
+
+
+class Config:
+    # User Setting
+    SEED = 42
+    ROOT_DIR = Path(__file__).parent.absolute()
+    DATA_DIR = ROOT_DIR/'data'
+
+    # Training
+    GPU_LIST = [0, 1]
+    NUM_EPOCH = 20
+    BATCH_SIZE = 32
+    LEARNING_RATE = 1e-2
+    CRITERION = 'RMSE'
+    OPTIMIZER = 'AdamW'
+    LR_SCHEDULER = 'StepLR'
+
+    # Model & Dataset
+    INPUT_SIZE = 2
+    D_MODEL = 64
+    NUM_LAYER = 3
+    DROPOUT = 0.2
+    SEQ_LEN = 10
+
+    def __init__(self, seed=None):
+        print(type(self.DATA_DIR))
+        self.set_random_seed(seed)
+
+    def set_random_seed(self, seed):
+        if seed:
+            self.SEED = seed
+
+        pl.seed_everything(self.SEED)
--- custom/dataset.py
+++ custom/dataset.py
@@ -10,14 +10,14 @@ from torch.utils.data import Dataset
 class CustomDataset(Dataset):
     def __init__(
         self,
-        max_len=10,
+        seq_len=10,
     ):
-        xs = self.create_sequence(max_len)
+        xs = self.create_sequence(seq_len)
 
         self.xs = torch.tensor(xs, dtype=torch.float)
 
-    def create_sequence(self, max_len):
-        sequence = [[i + j for j in range(max_len)] for i in range(max_len)]
+    def create_sequence(self, seq_len):
+        sequence = [[i + j for j in range(seq_len)] for i in range(20)]
         return sequence
 
     def __len__(self):
@@ -28,7 +28,7 @@ class CustomDataset(Dataset):
 
 
 if __name__ == '__main__':
-    test_dataset = CustomDataset(3)
+    test_dataset = CustomDataset(seq_len=3)
     print(f'Length: {len(test_dataset)}')
     print(f'Index 0: {test_dataset[0]}')
     print(f'Index 1: {test_dataset[1]}')
