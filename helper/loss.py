e40f9a66e060481187c0f1cfd2de96b0ca40d02d
sehandev <sehandev@gmail.com>
2021-10-05 16:03:02 +0900

    FEAT: loss

---
+++ helper/loss.py
@@ -0,0 +1,16 @@
+# Standard
+
+# PIP
+import torch
+import torch.nn as nn
+
+# Custom
+
+
+class RMSELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mse = nn.MSELoss()
+
+    def forward(self, yhat, y):
+        return torch.sqrt(self.mse(yhat, y))
